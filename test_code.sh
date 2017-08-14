ea643e5eaade6fe56d646568d44e4a95c5b9876b
Felix Berkenkamp <fberkenkamp@gmail.com>
2017-08-14 09:28:51 +0200

    Switch to pytest

--- test_code.sh
+++ test_code.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 
 module="safeopt"
@@ -17,19 +18,24 @@ get_script_dir () {
 
 # Change to script root
 cd $(get_script_dir)
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 # Run style tests
-echo "Running style tests"
-flake8 $module --exclude test*.py,__init__.py --ignore=E402,W503 --show-source
+echo -e "${GREEN}Running style tests.${NC}"
+flake8 $module --exclude test*.py,__init__.py --ignore=E402,E731,W503 --show-source
 
 # Ignore import errors for __init__ and tests
 flake8 $module --filename=__init__.py,test*.py --ignore=F,E402,W503 --show-source
 
-# Run unit tests
-echo "Running unit tests"
-nosetests --with-doctest $module
+echo -e "${GREEN}Testing docstring conventions.${NC}"
+# Test docstring conventions
+# pydocstyle $module --convention=numpy
 
-# Running coverage
-echo "Running coverage..."
-coverage run -m $module.test
-coverage report -m
+# Run unit tests
+echo -e "${GREEN}Running unit tests.${NC}"
+pytest --doctest-modules --cov --cov-fail-under=80 $module
+
+# Export html
+coverage html
+
